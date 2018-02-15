a588dbe4b813c4701334094812aed865b3042165
LuizPauloDamaceno <luizpaulodamaceno@live.com>
2018-02-14 23:23:15 -0200

    Audio processor: Overclock to 12.228MHz!
    Takes maximum performance <3

--- sound/soc/msm/msm8952.c
+++ sound/soc/msm/msm8952.c
@@ -47,8 +47,7 @@
 #define TER_MI2S_ID	(1 << 2)
 #define QUAT_MI2S_ID	(1 << 3)
 #define QUIN_MI2S_ID	(1 << 4)
-
-#define DEFAULT_MCLK_RATE 9600000
+#define DEFAULT_MCLK_RATE 12288000
 #if defined(CONFIG_C3N_SMB358)
 #define AW8736_MODE 5
 #elif defined(CONFIG_C3B_BQ2560X)
