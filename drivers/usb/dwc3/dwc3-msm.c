3f26e938207fef88144b156fe4994c51b83894cd
sunnyraj84348 <sunnyraj84348@gmail.com>
2018-05-31 17:12:40 +0000

    increase charging current

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -55,13 +55,13 @@
 #include "debug.h"
 #include "xhci.h"
 
-#if defined(CONFIG_D1_ROSY)
 #define DWC3_IDEV_CHG_MAX 2000
-#else
-#define DWC3_IDEV_CHG_MAX 1500
-#endif
-#define DWC3_HVDCP_CHG_MAX 1800
-#define DWC3_PROPRIETARY_CHG_MAX 1000
+#define DWC3_HVDCP_CHG_MAX 2000
+#define DWC3_PROPRIETARY_CHG_MAX 2000
+#define DWC3_WAKEUP_SRC_TIMEOUT 5000
+
+#define MICRO_5V    5000000
+#define MICRO_9V    9000000
 
 /* AHB2PHY register offsets */
 #define PERIPH_SS_AHB2PHY_TOP_CFG 0x10
