d9e250b3210b4f9623a97a3f10a90512fddecee2
sunnyraj84348 <sunnyraj84348@gmail.com>
2018-05-31 17:34:52 +0000

    fix compiling error

--- include/linux/usb/msm_hsusb.h
+++ include/linux/usb/msm_hsusb.h
@@ -124,6 +124,7 @@ enum msm_usb_phy_type {
 #define IDEV_CHG_MAX	2000
 #define IUNIT		100
 #define IDEV_HVDCP_CHG_MAX	2000
+#define IDEV_CHG_MIN    2000
 
 /**
  * Different states involved in USB charger detection.
