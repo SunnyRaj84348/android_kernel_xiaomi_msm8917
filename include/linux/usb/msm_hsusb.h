b91289ae773c1c94008ed65e85b66e3330c30b61
sunnyraj84348 <sunnyraj84348@gmail.com>
2018-07-04 15:50:54 +0000

    Increased Charging Current

--- include/linux/usb/msm_hsusb.h
+++ include/linux/usb/msm_hsusb.h
@@ -120,10 +120,10 @@ enum msm_usb_phy_type {
 	QUSB_ULPI_PHY,
 };
 
-#define IDEV_CHG_MAX	1000
+#define IDEV_CHG_MAX	2000
 #define IUNIT		100
-#define IDEV_HVDCP_CHG_MAX	1800
-#define IDEV_CHG_MIN 500
+#define IDEV_HVDCP_CHG_MAX	2000
+#define IDEV_CHG_MIN 2000
 
 /**
  * Different states involved in USB charger detection.
