97eca175edfa7e8994d1b67520639baf9d893698
mendigodepijama <mendigodepijama@gmail.com>
2019-01-16 12:06:49 -0200

    Fix GCam blinking focus issue

--- drivers/media/platform/msm/camera_v2/sensor/flash/msm_led_i2c_trigger.c
+++ drivers/media/platform/msm/camera_v2/sensor/flash/msm_led_i2c_trigger.c
@@ -408,10 +408,13 @@ static int32_t msm_led_get_dt_data(struct device_node *of_node,
 		return -EINVAL;
 	}
 
-	fctrl->flashdata = kzalloc(sizeof(fctrl->flashdata),
-				GFP_KERNEL);
-	if (!fctrl->flashdata)
+	fctrl->flashdata = kzalloc(sizeof(
+		struct msm_camera_sensor_board_info),
+		GFP_KERNEL);
+	if (!fctrl->flashdata) {
+		pr_err("%s failed %d\n", __func__, __LINE__);
 		return -ENOMEM;
+	}
 
 	flashdata = fctrl->flashdata;
 	power_info = &flashdata->power_info;
@@ -443,6 +446,7 @@ static int32_t msm_led_get_dt_data(struct device_node *of_node,
 		rc = 0;
 	}
 
+	fctrl->pinctrl_info.use_pinctrl = false;
 	fctrl->pinctrl_info.use_pinctrl = of_property_read_bool(of_node,
 						"qcom,enable_pinctrl");
 	if (of_get_property(of_node, "qcom,flash-source", &count)) {
@@ -494,8 +498,10 @@ static int32_t msm_led_get_dt_data(struct device_node *of_node,
 		power_info->gpio_conf =
 			 kzalloc(sizeof(struct msm_camera_gpio_conf),
 				 GFP_KERNEL);
-		if (!power_info->gpio_conf)
-			return -ENOMEM;
+		if (!power_info->gpio_conf) {
+			rc = -ENOMEM;
+			return rc;
+		}
 		gconf = power_info->gpio_conf;
 
 		gpio_array_size = of_gpio_count(of_node);
@@ -503,8 +509,8 @@ static int32_t msm_led_get_dt_data(struct device_node *of_node,
 
 		if (gpio_array_size) {
 			gpio_array =
-				kcalloc(gpio_array_size, sizeof(uint16_t),
-					 GFP_KERNEL);
+				kzalloc((sizeof(uint16_t) * gpio_array_size),
+					GFP_KERNEL);
 			if (!gpio_array) {
 				rc = -ENOMEM;
 				goto ERROR4;
