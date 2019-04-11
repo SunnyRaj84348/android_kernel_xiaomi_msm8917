272f3afeb5bac0a9bb394896a812bf0a53b7c9da
Danny Lin <danny@kdrag0n.dev>
2019-04-10 17:13:50 -0700

    net: ipc_router: Don't allocate memory for logging context if disabled
    ipc_log_context_create will always return NULL if CONFIG_IPC_LOGGING is
    disabled. Don't bother allocating memory for the log context only to
    free it immediately after ipc_log_context_create context is called since
    the result will always be NULL anyway.

--- net/ipc_router/ipc_router_core.c
+++ net/ipc_router/ipc_router_core.c
@@ -3991,6 +3991,8 @@ static void debugfs_init(void) {}
 static void *ipc_router_create_log_ctx(char *name)
 {
 	struct ipc_rtr_log_ctx *sub_log_ctx;
+	if (!IS_ENABLED(CONFIG_IPC_LOGGING))
+		return NULL;
 
 	sub_log_ctx = kmalloc(sizeof(struct ipc_rtr_log_ctx),
 				GFP_KERNEL);
