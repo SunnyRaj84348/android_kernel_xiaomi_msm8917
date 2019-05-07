a2257f2bea061f776483fec3bd23fe94fcf17b6b
sunnyraj84348 <sunnyraj84348@gmail.com>
2019-05-07 03:30:36 +0000

    Revert "HACK: selinux: do not require CONFIG_AUDIT"
    This reverts commit ea2409ed82bef545eeec2dbf1fb06ab5f4c7c439.

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -101,7 +101,6 @@ static inline int avc_hash(u32 ssid, u32 tsid, u16 tclass)
 	return (ssid ^ (tsid<<2) ^ (tclass<<4)) & (AVC_CACHE_SLOTS - 1);
 }
 
-#ifdef CONFIG_AUDIT
 /**
  * avc_dump_av - Display an access vector in human-readable form.
  * @tclass: target security class
@@ -168,7 +167,6 @@ static void avc_dump_query(struct audit_buffer *ab, u32 ssid, u32 tsid, u16 tcla
 	BUG_ON(tclass >= ARRAY_SIZE(secclass_map));
 	audit_log_format(ab, " tclass=%s", secclass_map[tclass-1].name);
 }
-#endif
 
 /**
  * avc_init - Initialize the AVC.
@@ -703,7 +701,6 @@ out:
 	return node;
 }
 
-#ifdef CONFIG_AUDIT
 /**
  * avc_audit_pre_callback - SELinux specific information
  * will be called by generic audit code
@@ -738,7 +735,6 @@ static void avc_audit_post_callback(struct audit_buffer *ab, void *a)
 				 ad->selinux_audit_data->result ? 0 : 1);
 	}
 }
-#endif
 
 /* This is the slow part of avc audit with big stack footprint */
 noinline int slow_avc_audit(u32 ssid, u32 tsid, u16 tclass,
@@ -775,9 +771,7 @@ noinline int slow_avc_audit(u32 ssid, u32 tsid, u16 tclass,
 
 	a->selinux_audit_data = &sad;
 
-#ifdef CONFIG_AUDIT 
 	common_lsm_audit(a, avc_audit_pre_callback, avc_audit_post_callback);
-#endif
 	return 0;
 }
 
